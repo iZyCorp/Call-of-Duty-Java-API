116110d7832b6a9665f58c38424396e5b8cf80b4
iZy <izyeeclipse@gmail.com>
2025-06-25 11:19:29 +0200

    ♻️Refactoring auth method

--- src/main/java/io/github/izycorp/codapi/query/RequestManager.java
+++ src/main/java/io/github/izycorp/codapi/query/RequestManager.java
@@ -4,7 +4,7 @@ import io.github.izycorp.codapi.events.Listener;
 import io.github.izycorp.codapi.events.components.ErrorInRequestEvent;
 import io.github.izycorp.codapi.events.components.PostRequestEvent;
 import io.github.izycorp.codapi.events.components.PreRequestEvent;
-import io.github.izycorp.codapi.exceptions.MoonViolationException;
+import io.github.izycorp.codapi.exceptions.CodRequestException;
 import okhttp3.*;
 
 import java.util.Objects;
@@ -23,7 +23,8 @@ public class RequestManager {
     /**
      * The base URL of the API
      */
-    private final String urlPrefix = "https://my.callofduty.com/api/papi-client/";
+    public static final String BASE_URL = "https://www.callofduty.com/api/papi-client/";
+    public static final String PROFILE_BASE_URL = "https://profile.callofduty.com/";
 
     /**
      * The User-Agent used to send requests
@@ -36,7 +37,7 @@ public class RequestManager {
     private final String baseCookie = "new_SiteId=cod;ACT_SSO_LOCALE=en_US;country=US;";
 
     /**
-     * A fake XSRF-TOKEN, this is basically used against CSRF attacks. A proper XCSRF token can be fetched from a request
+     * A fake XSRF-TOKEN, this is basically used against CSRF attacks. A proper XCSRF token can be fetched from a request,
      * but this step is not necessary since we can use a fake one.
      */
     private final String fakeXCSRF = "3844e7b2-ac07-4c97-8c72-0fa9f43fdd26";
@@ -51,6 +52,8 @@ public class RequestManager {
             .add("X-CSRF-TOKEN", fakeXCSRF)
             .add("User-Agent", userAgent)
             .add("Cookie", baseCookie);
+
+    private final Headers.Builder authHeader = baseHeader;
 
     /**
      * An OkHttpClient object used to send requests
@@ -76,16 +79,16 @@ public class RequestManager {
      *
      * @param client : A valid OkHttpClient
      */
-    public RequestManager(OkHttpClient client) {
+    public RequestManager(final OkHttpClient client) {
         this.client = client;
     }
 
     /**
-     * Constructor with only Listener as parameter
+     * Constructor with only Listener as a parameter
      *
      * @param listener : A class that extends Listener
      */
-    public RequestManager(Listener listener) {
+    public RequestManager(final Listener listener) {
         this();
         this.attachedListener = listener;
     }
@@ -96,7 +99,7 @@ public class RequestManager {
      * @param listener : A class that extends Listener
      * @param client   : A valid OkHttpClient
      */
-    public RequestManager(Listener listener, OkHttpClient client) {
+    public RequestManager(final Listener listener, final OkHttpClient client) {
         this(client);
         this.attachedListener = listener;
     }
@@ -108,10 +111,10 @@ public class RequestManager {
      * @param providedHeader : headers to add to the request (it is the header built by {@link #authenticate(String)}
      * @return : Raw response body
      */
-    private String sendRequest(String url, Headers.Builder providedHeader) throws MoonViolationException {
-        boolean hasListener = Objects.nonNull(this.attachedListener);
+    private String sendRequest(final String baseUrl, final String url, final Headers.Builder providedHeader) throws CodRequestException {
+        final boolean hasListener = Objects.nonNull(this.attachedListener);
         // building url
-        final String queryUrl = urlPrefix + url;
+        final String queryUrl = baseUrl + url;
         // Send PreRequestEvent
         if (hasListener)
             attachedListener.callEvent(new PreRequestEvent(queryUrl, (providedHeader != null ? providedHeader.build() : null)));
@@ -122,16 +125,20 @@ public class RequestManager {
                 .headers(providedHeader.build())
                 .url(queryUrl)
                 .build();
-        try (Response response = client.newCall(request).execute()) {
+        try (final Response response = client.newCall(request).execute()) {
             // Send PostRequestEvent
             if (hasListener) attachedListener.callEvent(new PostRequestEvent(response));
             return Objects.requireNonNull(response.body()).string();
         } catch (Exception exception) {
             // Send ErrorInRequestEvent
             if (hasListener) attachedListener.callEvent(new ErrorInRequestEvent(exception));
-            else throw new MoonViolationException(exception.getMessage());
+            else throw new CodRequestException(exception.getMessage());
         }
         return null;
+    }
+
+    private String sendRequest(final String url, final Headers.Builder providedHeader) throws CodRequestException {
+        return this.sendRequest(BASE_URL, url, providedHeader);
     }
 
     /**
@@ -140,54 +147,60 @@ public class RequestManager {
      * @param url : url to send the request to
      * @return : Response object (body)
      */
-    public String sendRequest(String url) throws MoonViolationException {
+    public String sendRequest(final String url) throws CodRequestException {
         return sendRequest(url, baseHeader);
     }
 
     /**
-     * This method is similar to {@link #sendRequest(String, Headers.Builder authHeader)} but it throws a MoonViolationException in case you are not logged in.
+     * This method is similar to {@link #sendRequest(String, Headers.Builder authHeader)} but it throws a CodRequestException in case you are not logged in.
      * This is used for public or protected routes.
      *
      * @param url        : url to send request to
-     * @param authHeader : custom Header, throw a MoonViolationException if not valid
-     * @return String : response body
+     * @return String: response body
      */
-    public String sendRequestWithAuthentication(String url, Headers.Builder authHeader) throws MoonViolationException {
+    public String sendRequestWithAuthentication(final String url) throws CodRequestException {
         return sendRequest(url, authHeader);
+    }
+
+    public String sendRequestWithAuthentication(final String baseUrl, final String url) throws CodRequestException {
+        return sendRequest(baseUrl, url, authHeader);
     }
 
     /**
      * <p>
      * This method is used to authenticate a user and return the header to use for authenticated requests.
-     * see {@link #sendRequestWithAuthentication(String, Headers.Builder authHeader)}
+     * see {@link #sendRequestWithAuthentication(String)}
      * </p>
      *
      * <br>
      *
      * <b>IMPORTANT NOTE:</b>
-     * Since Activision implemented a captcha system to their authentication page and since there is no other 'proper'
+     * Since Activision implemented a captcha system to their authentication page, and since there is no other 'proper'
      * way to authenticate using an API route, you'll have to fetch your SSO token by yourself.
+     * SSO tokens expire every 14 days.
      * <br>
      * To do so, you can follow those steps:
      * <ol>
      *     <li>Go to <a href="https://profile.callofduty.com/cod/login">https://profile.callofduty.com/cod/login</a></li>
      *     <li>Authenticate using your credentials</li>
-     *     <li>Right click and inspect the page, go to 'Storage' then 'Cookies' and search for <strong>ACT_SSO_COOKIE</strong></li>
+     *     <li>Right-click and inspect the page, go to 'Storage' then 'Cookies' and search for <strong>ACT_SSO_COOKIE</strong></li>
      * </ol>
      * @param ssoToken : sso token to authenticate (Follow below steps to get it)
      * @return A valid header for authenticated requests
      */
-    public Headers.Builder authenticate(String ssoToken) {
+    public Headers.Builder authenticate(final String ssoToken) {
         if (ssoToken == null || ssoToken.trim().isEmpty()) {
             return baseHeader;
         }
 
-        return baseHeader
+        this.authHeader
                 .add("XSRF-TOKEN", fakeXCSRF)
                 .add("CSRF-TOKEN", fakeXCSRF)
                 .add("ACT_SSO_COOKIE", ssoToken)
                 .add("ACT_SSO_REMEMBER_ME", ssoToken)
                 .add("cookie", baseCookie + "ACT_SSO_COOKIE=" + ssoToken + ";XSRF-TOKEN=" + fakeXCSRF + ";API_CSRF_TOKEN=" + fakeXCSRF + ";ACT_SSO_EVENT=\"LOGIN_SUCCESS:1644346543228\";ACT_SSO_COOKIE_EXPIRY=1645556143194;comid=cod;ssoDevId=63025d09c69f47dfa2b8d5520b5b73e4;tfa_enrollment_seen=true;gtm.custom.bot.flag=human;");
+
+        return this.authHeader;
 
     }
 }
